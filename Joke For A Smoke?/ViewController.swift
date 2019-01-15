fb30d967b2ba50ff791a054efec3ec812fe47814
Matt Huston <matthuston1@aol.com>
2019-01-15 14:49:53 -0700

    tryed if statement for under 100 characters, didn't work

--- Joke For A Smoke?/ViewController.swift
+++ Joke For A Smoke?/ViewController.swift
@@ -26,26 +26,68 @@ class ViewController: UIViewController {
                 let swiftyJsonVar = JSON(responseData.result.value!)
                 let JsonData = swiftyJsonVar["data"]
                 let JsonChildren = JsonData["children"]
-//                let random = JsonChildren.randomElement()!
                 let randomIndex = Int(arc4random_uniform(UInt32(JsonChildren.count)))
-//                print(JsonChildren[randomIndex])
                 let random = JsonChildren[randomIndex]
                 let data = random["data"]
                 let jokeTitle = data["title"]
                     print(jokeTitle)
                 
                 self.JokeTitle?.text = ("\(jokeTitle)")
-//                print(data)
-//                        print(JsonChildren)
-//                print(random)
-//                    print(swiftyJsonVar)
-//                print(swiftyJsonVar["data"])
-//                let dataArray = swiftyJsonVar.data.children
+
                 
 
-                }
             }
+        }
+    }
+    
+    @IBAction func DadJoke(_ sender: Any) {
+        
+        Alamofire.request("https://www.reddit.com/r/dadjokes/hot.json").responseJSON { (responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                let JsonData = swiftyJsonVar["data"]
+                let JsonChildren = JsonData["children"]
+                let randomIndex = Int(arc4random_uniform(UInt32(JsonChildren.count)))
+                let random = JsonChildren[randomIndex]
+                let data = random["data"]
+                let jokeTitle = data["title"]
+                let jokeText = data["selftext"]
+                print(jokeTitle)
+                print(jokeText)
+                print(jokeText.count)
+                
+                self.JokeTitle?.text = ("\(jokeTitle)\n\n\(jokeText)")
+            }
+        }
+    }
+
+    @IBAction func Jokes(_ sender: Any) {
+        
+        Alamofire.request("https://www.reddit.com/r/Jokes/hot.json").responseJSON { (responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+//                print(swiftyJsonVar)
+                let JsonData = swiftyJsonVar["data"]
+                let JsonChildren = JsonData["children"]
+                let randomIndex = Int(arc4random_uniform(UInt32(JsonChildren.count)))
+                let random = JsonChildren[randomIndex]
+                let data = random["data"]
+                let jokeTitle = data["title"]
+                let jokeText = data["selftext"]
+                print(jokeTitle)
+                print(jokeText)
+                print(jokeText.count)
+                if jokeText.count < 100 {
+                    self.JokeTitle?.text = ("\(jokeTitle)\n\n\(jokeText)")
+                }
+               
+                
+                
+                
+                
+                
+            }
+        }
     }
     
 }
-
